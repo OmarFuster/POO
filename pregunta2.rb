59378264953a80d89635738d2b12a7f01db8babf
OmarFuster <73042777+OmarFuster@users.noreply.github.com>
2020-10-30 17:05:13 -0500

    Update pregunta2.rb

--- pregunta2.rb
+++ pregunta2.rb
@@ -1,18 +1,19 @@
 class Administrador
-	attr_reader :nombreAdministrador, :arregloPedido
+	attr_reader :nombreAdministrador, :arregloPedido, :arregloRecepcion
 	def initialize(nombreAdministrador)
 		@nombreAdministrador = nombreAdministrador
 		@arregloPedido = []
+		@arregloRecepcion = []
 	end
 
 	def registrarPedido(pedido)
-		arregloPedido.push(pedido)
+		@arregloPedido.push(pedido)
 	end
-
 
 	def registrarRecepcionPedido(recepcion,pedido)
-		
+		@arregloRecepcion.push(recepcion,pedido)
 	end
+
 end
 
 class Pedido
@@ -31,13 +32,23 @@ class Pedido
 			return "INTERMEDIO"
 		elsif prioridad >= 15 && prioridad < 30				
 			return "BAJO"
+		else
+			return "FUERA DE FECHA"
 		end
 	end
+
+
 end
 
 adm = Administrador.new("OMAR")
-p1 = Pedido.new(1,10,4,1000)
+p1 = Pedido.new(1,10,3,1000)
+p2 = Pedido.new(2,20,6,2000)
+p3 = Pedido.new(3,30,16,3000)
+# a)	Permitir que la empresa realice el registro de un Pedido a un proveedor y realizar el registro de 10 pedidos a un proveedor.
 adm.registrarPedido(p1)
+adm.registrarPedido(p2)
+adm.registrarPedido(p3)
+
 for i in adm.arregloPedido
-	puts i.codigo
+	puts "#{i}"
 end
